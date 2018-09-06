e1688a3b298f34c572ca233c117014c7fc87917c
JoJoJun <mojinxinzhu@163.com>
2018-09-06 23:07:20 +0800

    allow_host in settings

--- kindleServer/kindleServer/settings.py
+++ kindleServer/kindleServer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5u8gs03ooau!o=z%7)r(f%&na#-^gic!x7dxa)%d26x04z3lcz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
